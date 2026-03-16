f31c8ea912f329bcbeb10a24d49af005d1e6f884
Antony-AK <antonyak147@gmail.com>
2026-03-16 15:04:15 +0530

    git commit new patches updated

--- src/Admin/AdminSubscribers.jsx
+++ src/Admin/AdminSubscribers.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import AdminLayout from "../Admin/components/AdminLayout";
 
-// const API = "https://palms-backend-bwad.onrender.com/api/subscribers";
-const API = "http://localhost:5000/api/subscribers";
+const API = "https://palms-backend-bwad.onrender.com/api/subscribers";
+// const API = "http://localhost:5000/api/subscribers";
 
 
 const AdminSubscribers = () => {
